3306666e679ac3cf4d8d37bd7bc191f742b18789
HadiHosseinpour <hadi1998goodboy@gmail.com>
2021-12-19 09:24:25 +0330

    edit

--- routes/api.php
+++ routes/api.php
@@ -203,8 +203,8 @@ Route::get('/get-six-new-product', function(){
         /***| BANK ROUTES |***/
        
         Route::post('/user-pasargad-charge-result',     [BankController::class,             'pasargadBankChargeResult']); // TO BE TESTED! 
+        Route::get('/user-pasargad-payment-result',    [BankController::class,             'pasargadBankPaymentResult']); // TO BE TESTED!
     });   
-    Route::get('/user-pasargad-payment-result',    [BankController::class,             'pasargadBankPaymentResult']); // TO BE TESTED!
 
     /***| SEARCH ROUTES |***/
     Route::post('/search-autocomplete',                 [SearchController::class,           'getAutocomplete']); // OK!
